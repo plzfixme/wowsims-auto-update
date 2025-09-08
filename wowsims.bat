eb62c3d2ed377628a9dfab716bb1cc7f2d3b67ea
plzfixme <plzfixme@gmail.com>
2025-09-08 11:58:28 +0200

    Update wowsims.bat

--- wowsims.bat
+++ wowsims.bat
@@ -6,7 +6,7 @@ set VERSION_FILE=version.txt
 set ZIP_NAME=wowsimmop-windows.exe.zip
 set EXTRACT_DIR=wowsims\mop
 
-:: Чтение локальной версии
+:: Check local version 
 set LOCAL_VERSION=
 if exist "%VERSION_FILE%" (
   for /f "usebackq delims=" %%A in ("%VERSION_FILE%") do (
@@ -18,32 +18,32 @@ if exist "%VERSION_FILE%" (
 set LOCAL_VERSION=%LOCAL_VERSION: =%
 echo Local version: %LOCAL_VERSION%
 
-:: Получение последней версии с GitHub
+:: Get latest release version of wowsims
 for /f "delims=" %%v in (
   'powershell -command "(Invoke-RestMethod -Uri https://api.github.com/repos/%REPO%/releases/latest).tag_name"'
 ) do set LATEST_VERSION=%%v
 echo Latest version: %LATEST_VERSION%
 
-:: Проверка на необходимость обновления
+:: Check if update is needed
 if "%LOCAL_VERSION%"=="%LATEST_VERSION%" (
   goto run
 )
 
-:: Формирование URL последнего релиза для скачивания
+:: Form download URL of the latest release
 set DOWNLOAD_URL=https://github.com/%REPO%/releases/latest/download/%ZIP_NAME%
 echo Downloading from: %DOWNLOAD_URL%
 
-:: Скачивание архива
+:: Download archive
 powershell -command "Invoke-WebRequest -Uri '%DOWNLOAD_URL%' -OutFile '%ZIP_NAME%'"
 
-:: Удаление старой распакованной версии и распаковка новой
+:: Delete old extracted version and extract new one
 if exist "%EXTRACT_DIR%" rmdir /s /q "%EXTRACT_DIR%"
 powershell -command "Expand-Archive -Path '%ZIP_NAME%' -DestinationPath '%EXTRACT_DIR%' -Force"
 
-:: Удаление архива
+:: Delete archive
 del "%ZIP_NAME%"
 
-:: Сохранение новой версии в файл
+:: Save new version to file
 echo %LATEST_VERSION% > "%VERSION_FILE%"
 
 :run
